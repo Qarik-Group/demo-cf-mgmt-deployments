98cb5feb29e4a5e68863c00d8387995fa16e32df
Patryk Kozak <thehackbee@gmail.com>
2022-07-25 10:26:02 +0000

    Export configuration for dev environment

--- export-config.sh
+++ export-config.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+
+if credhub f | grep cf_mgmt_client_secret; then
+    echo "cf_mgmt_client_secret present!"
+else
+    echo "Can't find the cf_mgmt_client_secret"
+    echo "Please check if you are authenticated in CredHub, from bosh-deployment run 'genesis do [env] credhub-login'"
+    echo "Please check if cf-deployment was run with feature that creates this client [ex. ops/cf-mgmt-uaa-client.yml]"
+    exit 1
+fi
+
 ./cf-mgmt export-config\
   --system-domain=system.codex.starkandwayne.com\
-  --user-id=\
-  --client-secret=\
-
-# //TODO:
-# - check on the user id, where it should come from
-# - check on the client secret
-# - client in that context is the OAuth2 UAA client
+  --user-id=cf_mgmt_client\
+  --client-secret=$(credhub g -n /dev-bosh/dev-cf/cf_mgmt_client_secret | sed -n 's/value: //p')
